9182b443292c94ff0a2ee495d47426f569ab18a1
BriceWu <bricewu@qq.com>
2026-01-16 17:08:26 +0800

    add

--- api/bitget_perp_api.py
+++ api/bitget_perp_api.py
@@ -15,5 +15,17 @@ class BitgetPerpApi(AccountBase):
         self._logger = logger
         if logger is None:
             raise Exception("日志对象未初始化")
+        self._host_address = "api.bitget.com"
         self._secret_key = None
         self._format_symbol = self.format_symbol()
+
+    def format_symbol(self, symbol=None):
+        """
+        按照bitget的方式格式化交易对
+        :param symbol:交易对
+        :return:格式化后的交易对
+        """
+        if not symbol:
+            return self._symbol.replace('_', "").upper()
+        else:
+            return symbol.replace('_', "").upper()
