752f167124ab892a52a2d5e7d0337a08e3b08590
BriceWu <bricewu@qq.com>
2026-01-20 16:37:37 +0800

    add

--- volume_monitor.py
+++ volume_monitor.py
@@ -8,11 +8,17 @@ from api.bn_public_rest_api import BinancePublicPerpApi
 
 
 class VolumeMonitor(ZMBase):
-    def __init__(self, symbol):
+    def __init__(self, symbol, volume_rate):
+        """
+        初始化
+        :param symbol: 交易对
+        :param volume_rate: 量比
+        """
         ZMBase.__init__(self)
         self._symbol = symbol
         self._logger = zm_log.get_log(self._symbol)
         self._rest_api = None
+        self._volume_rate = volume_rate
 
     def init_params(self):
         self._rest_api = BinancePublicPerpApi(self._symbol, self._logger)
