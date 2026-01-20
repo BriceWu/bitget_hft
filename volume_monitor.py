ef40e2ba87300487dd751577d6d417faa47c745c
BriceWu <bricewu@qq.com>
2026-01-20 16:23:01 +0800

    add

--- volume_monitor.py
+++ volume_monitor.py
@@ -7,20 +7,15 @@ import zmqfsi.util.zm_log as zm_log
 
 
 class VolumeMonitor(ZMBase):
-    def __init__(self, symbol, logger):
+    def __init__(self, symbol):
         ZMBase.__init__(self)
         self._symbol = symbol
-        self._logger = logger
-        if logger is None:
-            raise Exception("日志对象未初始化")
+        self._logger = zm_log.get_log(self._symbol)
         self._host_address = "fapi.binance.com"
-        self._format_symbol = self.format_symbol()
-        self._https_client = None
-        self.init_https_connection()
+
 
 if __name__ == '__main__':
     RunEnv.set_run_env('test')
     _symbol = "doge_usdt"
-    _logger = zm_log.get_log(_symbol)
-    b = VolumeMonitor(_symbol, _logger)
+    b = VolumeMonitor(_symbol)
     b.get_continuous_klines()
