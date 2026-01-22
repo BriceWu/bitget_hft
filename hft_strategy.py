c868a7c588593921f745548b1f8c65b9830c9914
BriceWu <bricewu@qq.com>
2026-01-22 15:50:46 +0800

    add

--- hft_strategy.py
+++ hft_strategy.py
@@ -50,7 +50,7 @@ class HFTStrategy(ZMBase):
         self.send_wechat(self._mail_to, "Active Orders Start", "Active Orders Start")
         self._logger.info("active orders start")
         # 同时运行 task_1 和 task_2
-        await asyncio.gather(self._tgt_ws_api.start_ws(), self.start_hft())  # 创建任务
+        await asyncio.gather(self._bn_ws_api.start_ws(), self._bitget_ws_api.start_ws(), self.start_hft())  # 创建任务
 
     async def start_hft(self):
         while True:
