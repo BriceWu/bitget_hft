ba8c8369bca0c5392498dd129f7f1f1473deba7b
BriceWu <bricewu@qq.com>
2026-01-22 13:45:35 +0800

    add

---
+++ hft_strategy.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3.13
+# -*- coding:utf-8 -*-
+import traceback, socket, http.client, time, os, sys
+from zmqfsi.service.zm_base import ZMBase
+from zmqfsi.util.zm_env import RunEnv
+import zmqfsi.util.zm_log as zm_log
+
+
+class HFTStrategy(ZMBase):
+    def __init__(self, symbol, mark):
+        """
+        初始化
+        :param symbol: 交易对
+        """
+        ZMBase.__init__(self)
+        self._symbol = symbol
+        self._logger = zm_log.get_log(f'{os.path.basename(sys.argv[0])[:-3]}_{self._symbol}')
+        self._public_rest_api = None
+        self._volume_rate = volume_rate
+        self._trade_side = trade_side
+        self._base_vol = 0
+
+    def init_params(self):
+        try:
+            return
+        except Exception as e:
+            error_info = "%s,%s" % (e, traceback.format_exc())
+            self._logger.error(error_info)
+            self.send_wechat(self._mail_to, "HFT量监控异常", error_info)
+            raise
+
+    def start(self):
+        self.init_params()
+        last_time = time.time()
+        while True:
+            try:
+                last_time = self.pace_cycle(last_time, cyc_time=1)
+                pass
+            except (socket.timeout, http.client.RemoteDisconnected, http.client.CannotSendRequest)  as e:
+                err_msg = repr(e)
+                self._logger.error(err_msg)
+                self.send_wechat(self._mail_to, "VolumeMonitor Exception1", err_msg)
+                self._public_rest_api.init_https_connection()
+            except Exception as e:
+                error_info = "%s,%s" % (e, traceback.format_exc())
+                self._logger.error(error_info)
+                self.send_wechat(self._mail_to, 'VolumeMonitor Exception2', error_info)
+                # 重建http连接
+                self._public_rest_api.init_https_connection()
+                time.sleep(10)
+
+
+if __name__ == '__main__':
+    RunEnv.set_run_env('test')
+    _symbol = "doge_usdt"
+    b = HFTStrategy(_symbol, None, None)
+    b.start()
