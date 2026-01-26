795801e10a60ed10dce8ca2374bcc6a28ad6e021
BriceWu <bricewu@qq.com>
2026-01-26 14:23:11 +0800

    self.bn_sell -> self.sum_bn_sell

--- hft_strategy.py
+++ hft_strategy.py
@@ -218,9 +218,9 @@ class HFTStrategy(ZMBase):
             self._have_placed_order = 0.
             return
         if posi_side == 1:
-            pass
+            self._rest_api.make_close_order(p_price=self._bitget_ask_one, p_vol=posi_vol, p_side='sell')
         elif posi_side == -1:
-            pass
+            self._rest_api.make_close_order(p_price=self._bitget_bid_one, p_vol=posi_vol, p_side='buy')
         else:
             error_msg = f"异常的仓位方向:{posi_side}, 持仓量:{posi_vol}"
             self._logger.error(error_msg)
