fcbd296e39bfd5c249df63d25e46906822a848a3
BriceWu <bricewu@qq.com>
2026-01-27 16:05:17 +0800

    self.bn_sell -> self.sum_bn_sell

--- hft_strategy.py
+++ hft_strategy.py
@@ -233,7 +233,7 @@ class HFTStrategy(ZMBase):
         """
         if self._have_placed_order == 0.:
             return # 没有下单, 没有仓位
-        if time.time() - self._have_placed_order < 4*60:  # 2min
+        if time.time() - self._have_placed_order < 3.5*60:  # 2min
             return
         posi_vol, posi_side = await self.analysis_position_info()
         if posi_vol is None:
