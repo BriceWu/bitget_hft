cf4f944aeb361b0c1d3834796dee4ca240de83ab
BriceWu <bricewu@qq.com>
2026-01-28 15:13:43 +0800

    add

--- api/bitget_perp_api.py
+++ api/bitget_perp_api.py
@@ -84,15 +84,16 @@ class BitgetPerpApi(AccountBase):
         body = f'{{"symbol":"{self._format_symbol}","productType":"USDT-FUTURES","marginMode":"isolated","marginCoin":"USDT","size":{p_vol},"price":"{p_price}","side":"{p_side}","orderType":"limit","clientOid":"{p_client_id}"}}'
         return self.http_post("/api/v2/mix/order/place-order", body)
 
-    def make_close_order(self, p_price, p_vol, p_side):
+    def make_close_order(self, p_price, p_vol, p_side, p_client_id):
         """
         开仓
         :param p_price:
         :param p_vol:
         :param p_side:
+        :param p_client_id:
         :return:
         """
-        body = f'{{"symbol":"{self._format_symbol}","productType":"USDT-FUTURES","marginMode":"isolated","marginCoin":"USDT","size":{p_vol},"price":"{p_price}","side":"{p_side}","orderType":"limit","reduceOnly":"YES"}}'
+        body = f'{{"symbol":"{self._format_symbol}","productType":"USDT-FUTURES","marginMode":"isolated","marginCoin":"USDT","size":{p_vol},"price":"{p_price}","side":"{p_side}","orderType":"limit","reduceOnly":"YES","clientOid":"{p_client_id}"}}'
         return self.http_post("/api/v2/mix/order/place-order", body)
 
     def cancel_order(self, p_client_id):
